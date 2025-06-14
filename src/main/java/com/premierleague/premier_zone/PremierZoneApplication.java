ef0b1ef1ffa3492d6e7b4ffefb4130023cdea784
Sandu Guzun <sanduguzun762@gmail.com>
2025-06-14 16:08:44 +0300

    commit

--- src/main/java/com/premierleague/premier_zone/PremierZoneApplication.java
+++ src/main/java/com/premierleague/premier_zone/PremierZoneApplication.java
@@ -3,11 +3,26 @@ package com.premierleague.premier_zone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class PremierZoneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PremierZoneApplication.class, args);
+		String url = "jdbc:mysql://127.0.0.1:3306/premier_league";
+		String user = "root";
+		String password = "parola";
+
+		try {
+			Connection connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Conexiune reușită!");
+			connection.close();
+		} catch (SQLException e) {
+			System.out.println("Eroare la conectare: " + e.getMessage());
+		}
 	}
 
 }
